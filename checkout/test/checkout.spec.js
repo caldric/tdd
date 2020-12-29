eec6cdfb46a97f76e2b1d1b5efa848f1eb2438d0
caldric <cvaldric@gmail.com>
2020-12-29 17:36:11 -0500

    Red: Price item addition

--- checkout/test/checkout.spec.js
+++ checkout/test/checkout.spec.js
@@ -5,4 +5,9 @@ describe('Checkout cart', () => {
   it('Creates an instance of the Checkout class', () => {
     const checkout = new Checkout()
   })
+
+  it('Adds item price', () => {
+    const checkout = new Checkout()
+    checkout.addItemPrice('a', 1)
+  })
 })
