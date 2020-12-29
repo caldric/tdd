43369a51c705d3e631af40cf077918ae249d39b5
caldric <cvaldric@gmail.com>
2020-12-29 17:39:38 -0500

    Refactor: Remove duplicate test
    The 'adds item price' test implies that a Checkout class can be created,
    thus making the latter test redundant.

--- checkout/test/checkout.spec.js
+++ checkout/test/checkout.spec.js
@@ -2,10 +2,6 @@ const expect = require('chai').expect
 const Checkout = require('../checkout')
 
 describe('Checkout cart', () => {
-  it('Creates an instance of the Checkout class', () => {
-    const checkout = new Checkout()
-  })
-
   it('Adds item price', () => {
     const checkout = new Checkout()
     checkout.addItemPrice('a', 1)
