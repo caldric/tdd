278cd0809abe96be30a85ecc32e8d646ccebed38
caldric <cvaldric@gmail.com>
2020-12-29 17:26:42 -0500

    Red: Checkout class instance creation

---
+++ checkout/test/checkout.spec.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect
+
+describe('Checkout cart', () => {
+  it('Creates an instance of the Checkout class', () => {
+    const checkout = new Checkout()
+  })
+})
--- checkout/test/sample.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect
-
-describe('sample', () => {
-  it('expects true to be true', () => {
-    expect(true).to.equal(true)
-  })
-})
