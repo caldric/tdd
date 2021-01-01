b3798a79d404e0fa36e38b80b0227a028f040205
caldric <cvaldric@gmail.com>
2021-01-01 18:38:51 -0500

    Refactor

--- sinon/test/getUsers.spec.js
+++ sinon/test/getUsers.spec.js
@@ -10,17 +10,25 @@ chai.should()
 chai.use(sinonChai)
 
 describe('Get users', () => {
+  let spy
+
+  beforeEach(() => {
+    spy = sinon.spy()
+    sinon.stub(request, 'get').callsFake((_, callback) => {
+      callback({}, { body: '{"users": ["user1", "user2"]}' })
+    })
+  })
+
+  afterEach(() => {
+    sinon.restore()
+  })
+
   it('Calls the callback once', () => {
-    const spy = sinon.spy()
     getUsers(spy)
     spy.should.have.been.calledOnce
   })
 
   it('Calls the correct URL', () => {
-    const spy = sinon.spy()
-    sinon.stub(request, 'get').callsFake((url, callback) => {
-      callback({}, { body: '{"users": ["user1", "user2"]}' })
-    })
     getUsers(spy)
     request.get.should.have.been.calledWith('https://www.mysite.com/api/users')
   })
