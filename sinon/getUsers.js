3cc945284a4e7bd4845ed1177c22ba513c5f4c2b
caldric <cvaldric@gmail.com>
2021-01-01 18:33:38 -0500

    Green: calls the correct URL

--- sinon/getUsers.js
+++ sinon/getUsers.js
@@ -1,3 +1,6 @@
+const request = require('request')
+
 module.exports = getUsers = (callback) => {
+  request.get('https://www.mysite.com/api/users', (err, res) => {})
   if (typeof callback === 'function') callback()
 }
