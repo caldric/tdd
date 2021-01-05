68cbafd47fd7673a191f841252d7ad44bff2cfaf
caldric <cvaldric@gmail.com>
2021-01-05 00:55:55 -0500

    Create test for rendering the App component

--- demo-react-app/src/App.test.js
+++ demo-react-app/src/App.test.js
@@ -1,6 +1,12 @@
-import { render, screen } from '@testing-library/react'
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('Renders <App /> without errors', () => {
+  // Do a shallow rendering of the App component
+  const wrapper = shallow(<App />)
+  expect(wrapper).toBeTruthy()
 })
