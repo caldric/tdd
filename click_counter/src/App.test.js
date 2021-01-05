6de7270d1986d35cbfca9300314afce92cfe9701
caldric <cvaldric@gmail.com>
2021-01-05 01:47:12 -0500

    Refactor: remove wrapper variable redundancy

--- click_counter/src/App.test.js
+++ click_counter/src/App.test.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
+
 import App from './App'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+let wrapper
+
+beforeEach(() => {
+  wrapper = shallow(<App />)
+})
+
 test('Renders <App /> without errors', () => {
-  const wrapper = shallow(<App />)
   const appComponent = wrapper.find("[data-test='component-app']")
   expect(appComponent.length).toBe(1)
 })
 
 test('Renders increment counter button', () => {
-  const wrapper = shallow(<App />)
   const incrementButton = wrapper.find("[data-test='increment-button']")
   expect(incrementButton.length).toBe(1)
 })
+
 // test('Renders counter display', () => {})
 // test('Counter starts at 0', () => {})
 // test('Clicking on button increments counter display', () => {})
