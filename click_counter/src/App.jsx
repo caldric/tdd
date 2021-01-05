6192c7fdabd0b88831ba982b2a9dbbd5607503e0
caldric <cvaldric@gmail.com>
2021-01-05 01:49:39 -0500

    Green: renders counter display

--- click_counter/src/App.jsx
+++ click_counter/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div data-test="component-app">
+      <h1 data-test="counter-display">0</h1>
       <button data-test="increment-button" type="button">
         Increment counter
       </button>
