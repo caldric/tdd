363676b8a420843ffec791eacebb1129f9118c79
caldric <cvaldric@gmail.com>
2021-01-07 06:03:37 -0500

    Refactor: put test in a describe block

--- jotto/src/helpers/index.test.ts
+++ jotto/src/helpers/index.test.ts
@@ -1,7 +1,9 @@
 import { getLetterMatchCount } from './'
 const secretWord = 'party'
 
-test('Returns correct count when there are no matching letters', () => {
-  const result = getLetterMatchCount('bones', secretWord)
-  expect(result).toBe(0)
+describe('getLetterMatchCount', () => {
+  test('Returns correct count when there are no matching letters', () => {
+    const result = getLetterMatchCount('bones', secretWord)
+    expect(result).toBe(0)
+  })
 })
