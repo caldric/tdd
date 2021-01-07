561a3ae03c8a3e15292d4eca30d513093ba7f89d
caldric <cvaldric@gmail.com>
2021-01-07 09:07:58 -0500

    Green: Returns true for "CORRECT_GUESS" action

--- jotto/src/reducers/success.ts
+++ jotto/src/reducers/success.ts
@@ -1,8 +1,12 @@
+import { actionTypes } from '../actions'
+
 const successReducer = (
   state: boolean = false,
   action: ActionTypes
 ): boolean => {
   switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true
     default:
       return state
   }
