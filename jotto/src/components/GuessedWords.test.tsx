67f352ae9201e0d2927e34391ac0028af311115d
caldric <cvaldric@gmail.com>
2021-01-07 04:37:34 -0500

    Red: renders without errors

--- jotto/src/components/GuessedWords.test.tsx
+++ jotto/src/components/GuessedWords.test.tsx
@@ -3,4 +3,12 @@ import { shallow } from 'enzyme'
 import { findByTestAttr } from '../../tests/testUtils'
 import GuessedWords from './GuessedWords'
 
-test('', () => {})
+const setup = (props = {}) => {
+  return shallow(<GuessedWords {...props} />)
+}
+
+test('Renders without errors', () => {
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, 'component-guessed-words')
+  expect(component.length).toBe(1)
+})
