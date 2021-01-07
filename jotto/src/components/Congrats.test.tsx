0c27b54635daa628ed58f2bf7079815c30d75f96
caldric <cvaldric@gmail.com>
2021-01-07 04:22:54 -0500

    Refactor: move Enzyme setup to setupTests.ts

--- jotto/src/components/Congrats.test.tsx
+++ jotto/src/components/Congrats.test.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme'
 import { findByTestAttr } from '../../tests/testUtils'
 import Congrats from './Congrats'
-
-Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 const setup = (props = {}) => {
   return shallow(<Congrats {...props} />)
