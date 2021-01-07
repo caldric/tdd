ee6bcebca7a139c7c5a5083868529295749582d2
caldric <cvaldric@gmail.com>
2021-01-07 09:20:54 -0500

    Create Input component and test

---
+++ jotto/src/components/Input.test.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { findByTestAttr } from '../../tests/testUtils'
+import Input from './Input'
+
+test('', () => {})
---
+++ jotto/src/components/Input.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+interface Props {}
+
+const Input: React.FC<Props> = () => {
+  return <div></div>
+}
+
+const mapStateToProps = (state) => {
+  return {}
+}
+
+export default connect(mapStateToProps)(Input)
