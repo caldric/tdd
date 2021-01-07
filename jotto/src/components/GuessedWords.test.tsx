ddb67a8848800b64eab5f43bf29208e9ebf09e12
caldric <cvaldric@gmail.com>
2021-01-07 04:29:50 -0500

    Add boilerplate to component and test

---
+++ jotto/src/components/GuessedWords.test.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { findByTestAttr } from '../../tests/testUtils'
+import GuessedWords from './GuessedWords'
+
+test('', () => {})
---
+++ jotto/src/components/GuessedWords.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const GuessedWords: React.FC = () => {
+  return <div></div>
+}
+
+export default GuessedWords
