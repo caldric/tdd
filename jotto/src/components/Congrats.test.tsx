51f1364b4598609c33ea2c28fae5aa17e256f42b
caldric <cvaldric@gmail.com>
2021-01-07 03:47:34 -0500

    Create test for Congrats component

---
+++ jotto/src/components/Congrats.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+import { findByTestAttr } from '../../tests/testUtils'
+import Congrats from './Congrats'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('', () => {})
