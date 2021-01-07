56f4d56dd521ef8f74b3ad1092f156102cbf2849
caldric <cvaldric@gmail.com>
2021-01-07 09:58:43 -0500

    Refactor: move redundancies to beforeEach function

--- jotto/src/components/Input.test.tsx
+++ jotto/src/components/Input.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, ShallowWrapper } from 'enzyme'
 import { findByTestAttr, storeFactory } from '../../tests/testUtils'
 import Input from './Input'
 
@@ -14,14 +14,17 @@ const setup = (initialState: RootState) => {
 // setup({ success: false }) // for debugging purposes
 
 describe('No guesses have been made', () => {
+  let wrapper: ShallowWrapper
+  beforeEach(() => {
+    wrapper = setup({ success: false })
+  })
+
   test('Renders component without errors', () => {
-    const wrapper = setup({ success: false })
     const component = findByTestAttr(wrapper, 'component-input')
     expect(component.length).toBe(1)
   })
 
   test('Renders input box', () => {
-    const wrapper = setup({ success: false })
     const inputBox = findByTestAttr(wrapper, 'input-box')
     expect(inputBox.length).toBe(1)
   })
