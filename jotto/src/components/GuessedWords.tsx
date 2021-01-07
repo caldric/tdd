e84050e0c18bddfc3098399f0b18e745e2aa9fe9
caldric <cvaldric@gmail.com>
2021-01-07 05:08:59 -0500

    Refactor: add helper methods for readability

--- jotto/src/components/GuessedWords.tsx
+++ jotto/src/components/GuessedWords.tsx
@@ -5,13 +5,18 @@ interface Props {
 }
 
 const GuessedWords: React.FC<Props> = ({ guesses = [] }) => {
+  const renderInstructions = (): JSX.Element | null =>
+    guesses.length === 0 ? (
+      <span data-test="guess-instructions">Instructions</span>
+    ) : null
+
+  const renderGuesses = (): JSX.Element | null =>
+    guesses.length > 0 ? <div data-test="guessed-words"></div> : null
+
   return (
     <div data-test="component-guessed-words">
-      {guesses.length === 0 ? (
-        <span data-test="guess-instructions">Instructions</span>
-      ) : (
-        <div data-test="guessed-words"></div>
-      )}
+      {renderInstructions()}
+      {renderGuesses()}
     </div>
   )
 }
