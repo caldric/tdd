ebee96e6e798c84c36d6a5ab37e7350fc432865a
caldric <cvaldric@gmail.com>
2021-01-07 09:41:18 -0500

    Set up input component and storeFactory test util

--- jotto/src/components/Input.test.tsx
+++ jotto/src/components/Input.test.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { findByTestAttr } from '../../tests/testUtils'
+import { findByTestAttr, storeFactory } from '../../tests/testUtils'
 import Input from './Input'
+import store from '../store'
+
+const setup = (initialState: RootState) => {
+  const store = storeFactory(initialState)
+  const wrapper = shallow(<Input store={store} />)
+    .dive()
+    .dive()
+  // console.log(wrapper.debug()) // for debugging purposes
+  return wrapper
+}
+// setup({ success: false }) // for debugging purposes
 
 test('', () => {})
--- jotto/tests/testUtils.ts
+++ jotto/tests/testUtils.ts
@@ -1,8 +1,14 @@
+import { createStore } from '@reduxjs/toolkit'
 import { ShallowWrapper } from 'enzyme'
+import rootReducer from '../src/reducers'
 
 export const findByTestAttr = (
   wrapper: ShallowWrapper,
   search: string
 ): ShallowWrapper => {
   return wrapper.find(`[data-test="${search}"]`)
+}
+
+export const storeFactory = (initialState: RootState) => {
+  return createStore(rootReducer, initialState)
 }
