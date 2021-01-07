7d82270b88ddd04597f4de5c0b0a74ddbb3d2e88
caldric <cvaldric@gmail.com>
2021-01-07 04:59:30 -0500

    Green: renders "guessed words" section

--- jotto/src/components/GuessedWords.tsx
+++ jotto/src/components/GuessedWords.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const GuessedWords: React.FC = () => {
+const GuessedWords: React.FC<Props> = () => {
   return (
     <div data-test="component-guessed-words">
       <span data-test="guess-instructions">Instructions</span>
+      <div data-test="guessed-words"></div>
     </div>
   )
 }
