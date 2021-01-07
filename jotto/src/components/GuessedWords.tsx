4e0efeb3336d73ac0de237b68a8e7c0d4545567f
caldric <cvaldric@gmail.com>
2021-01-07 04:44:38 -0500

    Green: renders instructions at initial load

--- jotto/src/components/GuessedWords.tsx
+++ jotto/src/components/GuessedWords.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const GuessedWords: React.FC = () => {
-  return <div data-test="component-guessed-words"></div>
+  return (
+    <div data-test="component-guessed-words">
+      <span data-test="guess-instructions">Instructions</span>
+    </div>
+  )
 }
 
 export default GuessedWords
