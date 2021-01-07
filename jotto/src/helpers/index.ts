856c57b979b4996f2b8ba1814435be60703f50bf
caldric <cvaldric@gmail.com>
2021-01-07 06:16:19 -0500

    Green: returns correct count with 3 matching letters

--- jotto/src/helpers/index.ts
+++ jotto/src/helpers/index.ts
@@ -1,3 +1,21 @@
 export const getLetterMatchCount = (guess: string, secret: string): number => {
-  return 0
+  // Initialize tracking variables
+  let matchingLetters = 0
+  const letters: any = {}
+
+  // Find the letter frequencies of the secret word
+  for (const char of secret) {
+    letters[char] = letters[char] + 1 || 1
+  }
+
+  // Count the letter matches between the guess and secret words
+  for (const char of guess) {
+    if (letters[char]) {
+      letters[char] -= 1
+      matchingLetters += 1
+    }
+  }
+
+  // Return value
+  return matchingLetters
 }
