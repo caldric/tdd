60cf53cbb075b6757151e94a50cc90d5fbb5d196
caldric <cvaldric@gmail.com>
2021-01-07 09:06:56 -0500

    Red: returns true for "CORRECT_GUESS" action

--- jotto/src/reducers/success.test.ts
+++ jotto/src/reducers/success.test.ts
@@ -5,3 +5,8 @@ test('Returns default initial state of "false" when generic action is passed', (
   const result = successReducer(undefined, { type: 'NONE' })
   expect(result).toBe(false)
 })
+
+test('Returns state of true upon receiving a "CORRECT_GUESS" action type', () => {
+  const result = successReducer(undefined, { type: actionTypes.CORRECT_GUESS })
+  expect(result).toBe(true)
+})
