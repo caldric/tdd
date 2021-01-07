110336d108a3447c204d8aac3f9262b2c3fb3380
caldric <cvaldric@gmail.com>
2021-01-07 08:55:59 -0500

    Move action interfaces and type aliases to types.d.ts

--- jotto/src/actions/index.ts
+++ jotto/src/actions/index.ts
@@ -2,12 +2,6 @@ export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
 }
 
-interface CorrectGuessAction {
-  type: typeof actionTypes.CORRECT_GUESS
-}
-
-type ActionTypes = CorrectGuessAction
-
 export const correctGuess = (): ActionTypes => {
   return { type: actionTypes.CORRECT_GUESS }
 }
--- jotto/src/types.d.ts
+++ jotto/src/types.d.ts
@@ -1,4 +1,15 @@
-interface Guess {
-  word: string
-  letterMatches: number
+import { actionTypes } from './actions'
+
+declare global {
+  interface Guess {
+    word: string
+    letterMatches: number
+  }
+
+  // Actions
+  interface CorrectGuessAction {
+    type: typeof actionTypes.CORRECT_GUESS
+  }
+
+  type ActionTypes = CorrectGuessAction
 }
