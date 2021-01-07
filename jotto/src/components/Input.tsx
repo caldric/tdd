0a66c261fefc8e2d808914f784eb376e12f95159
caldric <cvaldric@gmail.com>
2021-01-07 09:56:42 -0500

    Green: renders input box

--- jotto/src/components/Input.tsx
+++ jotto/src/components/Input.tsx
@@ -4,7 +4,11 @@ import { connect } from 'react-redux'
 interface Props {}
 
 const Input: React.FC<Props> = () => {
-  return <div data-test="component-input"></div>
+  return (
+    <div data-test="component-input">
+      <input data-test="input-box" type="text" />
+    </div>
+  )
 }
 
 const mapStateToProps = (state: RootState) => {
