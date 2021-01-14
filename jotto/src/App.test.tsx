05eb0405bd86dc5047477ee2ea9d4da7efbdf011
caldric <cvaldric@gmail.com>
2021-01-14 02:45:18 -0500

    Green: has access to success state

--- jotto/src/App.test.tsx
+++ jotto/src/App.test.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import { Provider } from 'react-redux'
-import { store } from './store'
+import { shallow } from 'enzyme'
+
+import { RootState } from './store'
+import { storeFactory } from '../tests/testUtils'
 import App from './App'
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   );
+const setup = (initialState: RootState | undefined) => {
+  const store = storeFactory(initialState)
+  // @ts-expect-error
+  const wrapper = shallow(<App store={store} />)
+    .dive()
+    .dive()
+  return wrapper
+}
 
-//   expect(getByText(/learn/i)).toBeInTheDocument();
-// });
-
-test('True to be equal to true', () => {
-  expect(true).toBe(true)
+describe('Redux properties', () => {
+  test('Has access to success state', () => {
+    const success = true
+    const wrapper = setup({ guesses: {}, secret: '', success })
+    const successProp = wrapper.instance().props.success
+    expect(successProp).toBe(success)
+  })
+  test('Has access to secret state', () => {})
+  test('Has access to guesses state', () => {})
 })
--- jotto/src/App.tsx
+++ jotto/src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
+import { RootState } from './store'
 import Congrats from './components/Congrats'
 import GuessedWords from './components/GuessedWords'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
-const App: React.FC = () => {
-  return (
-    <div className="container">
-      <h1>Jotto</h1>
-      <Congrats success={true} />
-      <GuessedWords
-        guesses={[
-          { word: 'train', letterMatches: 3 },
-          { word: 'agile', letterMatches: 1 },
-          { word: 'party', letterMatches: 5 },
-        ]}
-      />
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guesses={[
+            { word: 'train', letterMatches: 3 },
+            { word: 'agile', letterMatches: 1 },
+            { word: 'party', letterMatches: 5 },
+          ]}
+        />
+      </div>
+    )
+  }
 }
 
-export default App
+const mapStateToProps = (state: RootState) => {
+  const { success } = state
+  return { success }
+}
+
+export default connect(mapStateToProps)(App)
